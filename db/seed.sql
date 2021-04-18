3e0af4650d339e12e617913e15e03ed5a0e649cc
shangxian Piao <sj212131@hotmail.com>
2021-04-18 17:01:12 -0400

    updated the database

--- db/seed.sql
+++ db/seed.sql
@@ -22,12 +22,12 @@ VALUES
 USE Employee_ManagementDB;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES  ("John", "Doe", 1, NULL ),
+VALUES  ("John", "Doe", 1, 3 ),
 		("Mike", "Chan", 2, 1),
 		("Ashley", "Rodriguez", 3, NULL),
 		("Kevin", "Tupik", 4, 3),
-		("Malia", "Brown", 5, 3),
+		("Malia", "Brown", 5, NULL),
 		("Sarah", "Lourd", 6, NULL),
 		("Tom", "Allen", 7, 6),
-		("Christian", "Echenrode",8, 6);
+		("Christian", "Echenrode",8, 2);
 -- SELECT * FROM employee;
