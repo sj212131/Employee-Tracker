df442d82d8050796743c26e6f2d4a73e455dbc01
shangxian Piao <sj212131@hotmail.com>
2021-04-18 17:02:19 -0400

    updated the database and typo

--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,28 @@
 USE Employee_ManagementDB;
+
 INSERT INTO department (name)
- VALUES 
+	VALUES 
 		("Sales"), -- ID: 1
 		("Engineering"), -- ID: 2
 		("Accounting"),-- ID: 3
 		("Legal");-- ID: 4
--- SELECT * FROM department;
+	-- SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_ID) 
-VALUES 
-    ("Lead Sales", 100000, 1),
-    ("Salesperson", 80000, 1),
-    ("lead Engineer", 150000, 2),
-    ("Software Engineer", 120000, 2),
-    ("Accountant", 125000, 3),
-    ("Legal Team Lead", 250000 , 4),
-    ("Lawyer", 190000, 4),
-    ("Lead Engineer", 150000, 2);
--- SELECT * FROM role;
-
-USE Employee_ManagementDB;
+	VALUES 
+		("Lead Sales", 100000, 1),
+		("Salesperson", 80000, 1),
+		("lead Engineer", 150000, 2),
+		("Software Engineer", 120000, 2),
+		("Accountant", 125000, 3),
+		("Legal Team Lead", 250000 , 4),
+		("Lawyer", 190000, 4),
+		("Lead Engineer", 150000, 2);
+	-- SELECT * FROM role;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES  ("John", "Doe", 1, 3 ),
+	VALUES  
+		("John", "Doe", 1, 3 ),
 		("Mike", "Chan", 2, 1),
 		("Ashley", "Rodriguez", 3, NULL),
 		("Kevin", "Tupik", 4, 3),
@@ -30,4 +30,4 @@ VALUES  ("John", "Doe", 1, 3 ),
 		("Sarah", "Lourd", 6, NULL),
 		("Tom", "Allen", 7, 6),
 		("Christian", "Echenrode",8, 2);
--- SELECT * FROM employee;
+	-- SELECT * FROM employee;
